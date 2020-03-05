4974ed28d451de6a57fe67c68e2f0b62fda1ec11
Vinicius Fernandes <vnferna@gmail.com>
2020-03-05 11:05:32 -0300

    Melhorias nas logica e nas estrutura do codigo

--- versao_1_0/cookpit.ino.ino
+++ versao_1_0/cookpit.ino.ino
@@ -2,11 +2,11 @@
 
 //Teclas de ações
 word teclasMotores[] = {"KEY_F1", "KEY_F2", "KEY_F3", "KEY_F4"};
-char teclaFlaps = 'F';
-char teclaTremPouso = 'G';
+char teclaFlaps = 'f';
+char teclaTremPouso = 'g';
 word teclaEmbandeiramento = "KEY_F5";
-char teclaCompartimentoBomba = '';
-char teclaSerieBombas = '';
+char teclaCompartimentoBomba = 'y';
+char teclaSerieBombas = 't';
 //Configuração de pinos dos controle
 int pinoEmbandeiramento[] = {2, 3, 4, 5};
 int pinoFlaps = 6;
@@ -14,15 +14,13 @@ int pinoTremPouso = 7;
 int pinoPortaBombas = 8;
 int pinoBombas = 9;
 //Motores embandeirado
-int motorEmbandeirado[] = {0, 0, 0, 0};
+bool motorEmbandeirado[] = {false, false, false, false};
 //Estado dos flaps
 bool estadoFlaps = false;
 //Estado do trem de pouso
 bool estadoTremPouso = true;
 //Estado do trem de pouso
 bool estadoCompartimentoBomba = false;
-//estado do botao das bombas 
-bool estadoBombas = false;
 
 void setup(){
     Serial.begin(9600);
@@ -50,13 +48,9 @@ void loop(){
 //Rotina de Bombas
 //
 void liberarSerieBombas(){
-  if(digitalRead(pinoBombas) == HIGH && estadoBombas == false){
+  if(digitalRead(pinoBombas) == HIGH){
     Serial.println("Liberando Bombas!");
     Keyboard.write(teclaSerieBombas);
-    estadoBombas = true;
-  }
-  if(digitalRead(pinoBombas) == LOW && estadoBombas == true){
-    estadoBombas = false;
   }
 }
 
@@ -82,20 +76,17 @@ void abrirCompartimentoBombas(){
 //Rotina dos flaps
 //
 void verificaFlaps(){
-    if(digitalRead(pinoPortaBombas) == HIGH){
+    if(digitalRead(pinoFlaps) == HIGH && !estadoFlaps){
         //Se ele nao estiver ativo ira acionar
-        if(!estadoFlaps){
-            Serial.println("Ativando flaps!");
-            Keyboard.write(teclaFlaps);
-            estadoFlaps = true;
-        }
-    }else{
-        //Se ele estiver ativo ira desacionar
-        if(estadoFlaps){
-            Serial.println("Desativando flaps!");
-            Keyboard.write(teclaFlaps);
-            estadoFlaps = false;
-        }
+        Serial.println("Ativando flaps!");
+        Keyboard.write(teclaFlaps);
+        estadoFlaps = true;
+    }
+    //Se ele estiver ativo ira desacionar
+    if(digitalRead(pinoFlaps) == LOW && estadoFlaps){{
+        Serial.println("Desativando flaps!");
+        Keyboard.write(teclaFlaps);
+        estadoFlaps = false;
     }
 }
 
@@ -103,21 +94,19 @@ void verificaFlaps(){
 //Rotina do trem de pouso
 //
 void verificaTremPouso(){
-    if(digitalRead(pinoTremPouso) == HIGH){
+    if(digitalRead(pinoTremPouso) == HIGH && !estadoTremPouso){
         //Se ele nao estiver ativo ira acionar
-        if(!estadoTremPouso){
-            Serial.println("Abaixando trem de pouso!");
-            Keyboard.write(teclaTremPouso);
-            estadoTremPouso = true;
-        }
-    }else{
-        //Se ele estiver ativo ira desacionar
-        if(estadoTremPouso){
-            Serial.println("Recolhendo trem de pouso!");
-            Keyboard.write(teclaFlaps);
-            estadoTremPouso = false;
-        }
+        Serial.println("Abaixando trem de pouso!");
+        Keyboard.write(teclaTremPouso);
+        estadoTremPouso = true;
     }
+    //Se ele estiver ativo ira desacionar
+    if(digitalRead(pinoTremPouso) == LOW && estadoTremPouso){
+        Serial.println("Recolhendo trem de pouso!");
+        Keyboard.write(teclaFlaps);
+        estadoTremPouso = false;
+    }
+    
 }
 
 //
@@ -127,19 +116,15 @@ void verificaEmbandeiramento(){
     //Testa todos os motores para saber se precisam ser alterados
     int motor = 0;
     while(motor <= 3){
-      if(digitalRead(pinoEmbandeiramento[motor]) == HIGH){
-          if(motorEmbandeirado[motor] == 0){
-                //Se ele nao estiver embandeirado ira executar a rotina
-                Serial.println("Iniciando Embandeiramento motor:" + String(motor + 1));
-              embandeirarDesembandeirar(motor);
-          }
-        }else{
-            if(motorEmbandeirado[motor] == 1){
-                //Se ele nao estiver desembandeirado ira executar a rotina
-                Serial.println("Iniciando Desembandeiramento motor:" + String(motor + 1));
-              embandeirarDesembandeirar(motor);
-          }
-        }
+      if(digitalRead(pinoEmbandeiramento[motor]) == HIGH && !motorEmbandeirado[motor]){
+            //Se ele nao estiver embandeirado ira executar a rotina
+            Serial.println("Iniciando Embandeiramento motor:" + String(motor + 1));
+            embandeirarDesembandeirar(motor);
+      if(digitalRead(pinoEmbandeiramento[motor]) == LOW && motorEmbandeirado[motor]){
+            //Se ele nao estiver desembandeirado ira executar a rotina
+            Serial.println("Iniciando Desembandeiramento motor:" + String(motor + 1));
+            embandeirarDesembandeirar(motor);
+      }
         motor++;
     }
 }
@@ -148,17 +133,17 @@ void embandeirarDesembandeirar(int motorAlterado){
     //Define moteres que sofrerão a ação
     selecionaMotores(motorAlterado);
     //Define a ação
-    if(motorEmbandeirado[motorAlterado] == 1){
+    if(motorEmbandeirado[motorAlterado]){
         Serial.println("Acao de desembandeiramento motor:" + String(motorAlterado + 1));
         Keyboard.write(teclasMotores[motorAlterado]);
-        motorEmbandeirado[motorAlterado] = 0;
+        motorEmbandeirado[motorAlterado] = false;
     }else{
         Serial.println("Acao de embandeiramento motor:" + String(motorAlterado + 1));
         //Executa a ação
         Keyboard.write(teclaEmbandeiramento);
         //desativa motor embandeirado
         Keyboard.write(teclasMotores[motorAlterado]);
-        motorEmbandeirado[motorAlterado] = 1;
+        motorEmbandeirado[motorAlterado] = true;
     }
     //Reativa Motores validos
     selecionaMotores(motorAlterado);
